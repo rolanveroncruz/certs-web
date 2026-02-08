6e48c737010a788d233d42674d9c47d619c171b3
rolanveroncruz <114793101+rolanveroncruz@users.noreply.github.com>
2026-02-08 23:13:42 +0800

    digital signing page

--- src/app/digital-signing/digital-signing.ts
+++ src/app/digital-signing/digital-signing.ts
@@ -4,6 +4,8 @@ import {CreatingSecure} from './creatingsecure/creatingsecure';
 import {DSResources} from './ds-resources/resources';
 import {ContactUs} from '../landing/contact-us/contact-us';
 import {VideoPlayer} from '../video-player/video-player';
+import {TransformYourDocumentSecurity} from './transform-your-document-security/transform-your-document-security';
+import {StopWaitingForWetSignatures} from './stop-waiting-for-wet-signatures/stop-waiting-for-wet-signatures';
 
 @Component({
   selector: 'app-digital-signing',
@@ -12,7 +14,9 @@ import {VideoPlayer} from '../video-player/video-player';
     CreatingSecure,
     DSResources,
     ContactUs,
-    VideoPlayer
+    VideoPlayer,
+    TransformYourDocumentSecurity,
+    StopWaitingForWetSignatures
   ],
   templateUrl: './digital-signing.html',
   styleUrl: './digital-signing.scss'
---
+++ src/app/digital-signing/transform-your-document-security/transform-your-document-security.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transform-your-document-security',
+  imports: [],
+  templateUrl: './transform-your-document-security.html',
+  styleUrl: './transform-your-document-security.scss'
+})
+export class TransformYourDocumentSecurity {
+
+}
